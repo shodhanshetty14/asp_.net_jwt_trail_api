6e8c1405e15aba10ffb861077571826fa548af6d
shodhanshetty14 <shodhanshetty144@gmail.com>
2024-10-27 17:51:37 +0530

    added a send notification api

--- Jwt_Tutorial.api/Controllers/UserController.cs
+++ Jwt_Tutorial.api/Controllers/UserController.cs
@@ -1,7 +1,11 @@
 ﻿using Jwt_Tutorial.api.Models;
+using Jwt_Tutorial.api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,10 +18,12 @@ namespace Jwt_Tutorial.api.Controllers
     {
         public readonly JwtApiContext _dbcontext;
         public readonly IConfiguration _config;
-        public UserController(JwtApiContext dbcontext, IConfiguration config)
+        public readonly IEmailService _emailservice;
+        public UserController(JwtApiContext dbcontext, IConfiguration config, IEmailService emailservice)
         {
             _dbcontext = dbcontext;
             _config = config;
+            _emailservice = emailservice;
         }
 
 
@@ -38,6 +44,54 @@ namespace Jwt_Tutorial.api.Controllers
             return Ok(user);
         }
 
+        [HttpPost("send-notification")]
+        public async Task<IActionResult> SendNotification([FromBody] NotificationModel data)
+        {
+            await _emailservice.SendEmail(data.reciever, data.subject, data.body);
+            return Ok("message sent");
+        }
+
+        [HttpPost("change-password")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel data)
+        {
+            var loggedUser = User.FindFirstValue("Email");
+            int loggedId;
+            int.TryParse(User.FindFirstValue("UserId"), out loggedId); 
+            //Console.WriteLine(loggedUser + " "+ loggedId);
+            var user = await _dbcontext.Users.FindAsync(loggedId);
+            if(user == null)
+            {
+                return NotFound("User not found");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(data.oldPassword, user.Password))
+            {
+                return BadRequest("Invalid Password");
+            }
+            if(data.newPassword != data.confirmNewPassword)
+            {
+                return BadRequest("The passwords do not match!");
+            }
+            
+            var HashPass = BCrypt.Net.BCrypt.HashPassword(data.newPassword);
+            user.Password = HashPass;
+            await _dbcontext.SaveChangesAsync();
+            return Ok("Password Changed Successfully!");
+        }
 
     }
+
+    public class NotificationModel
+    {
+        public string reciever { get; set; } = string.Empty;
+        public string subject { get; set; } = string.Empty;
+        public string body { get; set; } = string.Empty;
+    }
+
+    public class ChangePasswordModel
+    {
+        public string oldPassword { get; set; } = string.Empty;
+        public string newPassword { get; set; } = string.Empty;
+        public string confirmNewPassword { get; set; } = string.Empty;
+    }
+
 }
